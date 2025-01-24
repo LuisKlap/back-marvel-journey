8adf4cf7018a7b666d4f81efa0f5f419c5f855b1
LuisKlap <luis_klap@outlook.com>
2025-01-23 21:19:05 -0300

    remove arquivo maven-wrapper.properties, ajusta nome do projeto no pom.xml e adiciona dependência spring-dotenv; atualiza propriedades de logging e diretório do dotenv nos arquivos de configuração

--- marvel-journey/src/main/java/com/marvel/marveljourney/MarvelJourneyApplication.java
+++ marvel-journey/src/main/java/com/marvel/marveljourney/MarvelJourneyApplication.java
@@ -2,10 +2,8 @@ package com.marvel.marveljourney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class MarvelJourneyApplication {
 
     public static void main(String[] args) {
