6e30e39e04525e44e97a1bbe2511160e15ac2bed
LuisKlap <luis_klap@outlook.com>
2025-01-26 23:15:17 -0300

    refatora teste de EmailService; melhora a legibilidade ao renomear variáveis e métodos, e adiciona tratamento de exceção para falha no envio de email

--- marvel-journey/src/test/java/com/marvel/marveljourney/service/EmailServiceTest.java
+++ marvel-journey/src/test/java/com/marvel/marveljourney/service/EmailServiceTest.java
@@ -1,22 +1,20 @@
 package com.marvel.marveljourney.service;
 
-import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class EmailServiceTest {
 
     @Mock
-    private JavaMailSender mailSender;
+    private JavaMailSender javaMailSender;
 
     @InjectMocks
     private EmailService emailService;
@@ -27,18 +25,24 @@ class EmailServiceTest {
     }
 
     @Test
-    void testSendVerificationEmail() throws MessagingException {
-        String to = "test@gmail.com";
+    void testSendVerificationEmailSuccess() {
+        String to = "test@example.com";
         String code = "123456";
 
         emailService.sendVerificationEmail(to, code);
 
-        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
-        verify(mailSender, times(1)).send(messageCaptor.capture());
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
+    }
 
-        SimpleMailMessage sentMessage = messageCaptor.getValue();
-        assertEquals(to, sentMessage.getTo()[0]);
-        assertEquals("Marvel Journey - Código de verificação", sentMessage.getSubject());
-        assertEquals("Seu código de verificação é: " + code, sentMessage.getText());
+    @Test
+    void testSendVerificationEmailFailure() {
+        String to = "test@example.com";
+        String code = "123456";
+
+        doThrow(new MailException("Erro ao enviar email") {}).when(javaMailSender).send(any(SimpleMailMessage.class));
+
+        emailService.sendVerificationEmail(to, code);
+
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
     }
 }
