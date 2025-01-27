15f16a13d6b5c983f9c01be1002ba7f52e8442b7
LuisKlap <luis_klap@outlook.com>
2025-01-26 21:59:49 -0300

    adiciona testes para validação de senhas; implementa casos de teste para senhas válidas e inválidas, incluindo critérios de complexidade

--- marvel-journey/src/test/java/com/marvel/marveljourney/util/PasswordValidatorUtilTest.java
+++ marvel-journey/src/test/java/com/marvel/marveljourney/util/PasswordValidatorUtilTest.java
@@ -1,6 +1,76 @@
 package com.marvel.marveljourney.util;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordValidatorUtilTest {
 
-    //To Do
+    private PasswordValidatorUtil passwordValidatorUtil;
+
+    @BeforeEach
+    void setUp() throws FileNotFoundException, IOException {
+        passwordValidatorUtil = new PasswordValidatorUtil();
+    }
+
+    @Test
+    void testValidPassword() {
+        String validPassword = "Valid1Password!";
+        assertTrue(passwordValidatorUtil.validate(validPassword));
+    }
+
+    @Test
+    void testInvalidPasswordTooShort() {
+        String invalidPassword = "Short1!";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testInvalidPasswordNoUpperCase() {
+        String invalidPassword = "nouppercase1!";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testInvalidPasswordNoLowerCase() {
+        String invalidPassword = "NOLOWERCASE1!";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testInvalidPasswordNoDigit() {
+        String invalidPassword = "NoDigitPassword!";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testInvalidPasswordNoSpecialCharacter() {
+        String invalidPassword = "NoSpecialCharacter1";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testInvalidPasswordWithWhitespace() {
+        String invalidPassword = "Invalid Password1!";
+        assertFalse(passwordValidatorUtil.validate(invalidPassword));
+    }
+
+    @Test
+    void testGetMessagesForInvalidPassword() {
+        String invalidPassword = "short";
+        List<String> messages = passwordValidatorUtil.getMessages(invalidPassword);
+        assertFalse(messages.isEmpty());
+    }
+
+    @Test
+    void testGetMessagesForValidPassword() {
+        String validPassword = "Valid1Password!";
+        List<String> messages = passwordValidatorUtil.getMessages(validPassword);
+        assertTrue(messages.isEmpty());
+    }
 }
