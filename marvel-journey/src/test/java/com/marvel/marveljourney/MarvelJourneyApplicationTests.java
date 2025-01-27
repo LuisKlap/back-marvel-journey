d00b3f2ce8d0e727d2b143d4c0858bc585649e66
LuisKlap <luis_klap@outlook.com>
2025-01-26 21:59:34 -0300

    adiciona testes para verificar o carregamento do contexto da aplicação e a execução do método main

--- marvel-journey/src/test/java/com/marvel/marveljourney/MarvelJourneyApplicationTests.java
+++ marvel-journey/src/test/java/com/marvel/marveljourney/MarvelJourneyApplicationTests.java
@@ -1,14 +1,29 @@
 package com.marvel.marveljourney;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class MarvelJourneyApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // Verifica se o contexto da aplicação carrega corretamente
+        assertNotNull(applicationContext, "O contexto da aplicação não deveria ser nulo");
+    }
+
+    @Test
+    void main() {
+        // Executa o método main da aplicação para garantir que ele não lança exceções
+        MarvelJourneyApplication.main(new String[] {});
     }
 }
